0d1525b669308575d3b0af7f5d3e3b8e974e5e57
Opilane TTHK <opilane@tthk.ee>
2021-10-20 09:50:38 +0300

    first commit

---
+++ Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma nimi");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta oma sünniaastat:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta oma perekonnanimi");
+            string lastName = Console.ReadLine();
+            
+            //lisa perekonna nimi
+            Greetings(userName, userAge, lastName);
+        }
+
+        private static void Greetings(string someString, int someInt, string someText)
+        {
+            Console.WriteLine($"Tere, {someString} {someText}!");
+            Console.WriteLine($"Oled {2021 - someInt}.");
+        }
+
+    }
+}
